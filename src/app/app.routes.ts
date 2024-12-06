6ca6c3edbefa18b1a4627065e9e32ce7d473ceb4
Ashu0330 <Himanshu.suman0076@gmail.com>
2024-12-06 16:04:28 +0530

    navigation bar component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { NavComponent } from './nav/nav.component';
 
 export const routes: Routes = [
     {
         path:'user',component:UserComponent
+    },
+    {
+        path:'nav',component:NavComponent
+    
     }
 ];
