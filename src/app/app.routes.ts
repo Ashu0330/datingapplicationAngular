66d7096696f4ab3cd3d6bbc4d7dd724dd7d456b6
Ashu0330 <himanshu.suman0076@gmail.com>
2024-12-09 01:14:48 +0530

    ading signup files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { NavComponent } from './nav/nav.component';
+import { SignUp } from './service/model/signup';
+import { SigninComponent } from '../signin/signin.component';
 
 export const routes: Routes = [
     {
-        path:'user',component:UserComponent
+        path: 'user', component: UserComponent
     },
     {
-        path:'nav',component:NavComponent
-    
+        path: 'nav', component: NavComponent
+    },
+    {
+        path: 'signup', component: SigninComponent
     }
 ];
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ApiResponse } from './model/apiresponse';
 import { usermodel } from './model/usemodel';
 import { loginmodel } from './model/loginmodel';
+import { SignUp } from './model/signup';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,12 @@ export class UserService {
     return this.http.get<ApiResponse<usermodel>>("https://localhost:7138/api/User/GetUsers")
 
   }
-  loginmethod(model:any) {
-    return this.http.post<ApiResponse<usermodel>>(`https://localhost:7138/api/User/Login`,model);
+  loginmethod(model: any) {
+    return this.http.post<ApiResponse<usermodel>>(`https://localhost:7138/api/User/Login`, model);
   }
 
+  SignUpMethod(model:any) {
+    return this.http.post<ApiResponse<boolean>>('https://localhost:7138/api/User/SignUp',model)
+  }
 
 }
